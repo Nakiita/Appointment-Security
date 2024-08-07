4d7d47be841a53e6db34c6cfa05b9310aa38b40a
unknown <nakitarayamajhi27@gmail.com>
2024-08-07 18:06:06 +0545

    Unnecessary comments removing

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,5 +4,4 @@ const connectDB = async () => {
   const { connection } = await mongoose.connect(process.env.DB_URL);
   console.log(`MongoDB is connected at:${connection.host}`.cyan.underline.bold);
 };
-//export
 module.exports = connectDB;
