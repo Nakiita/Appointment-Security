d7825b72879f0830be97c362e4ddc5996f0099a8
unknown <nakitarayamajhi27@gmail.com>
2024-08-07 18:05:58 +0545

    Unnecessary comments removing

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-//list of function
 const connectDB = async () => {
   const { connection } = await mongoose.connect(process.env.DB_URL);
   console.log(`MongoDB is connected at:${connection.host}`.cyan.underline.bold);
